62489a147955f14812e2f960e628a17796c23b5d
OMEGA0002 <167677115+OMEGA0002@users.noreply.github.com>
2024-05-24 19:20:08 +0800

    Add files via upload

---
+++ 第三课时/lx3.py
@@ -0,0 +1,8 @@
+d1={1,2,3}
+user=input("请输入你想了解的网络流行词：")
+if user in d1:
+    print("你查询的"+user+"含义如下")
+    print("显示流行语起源或示意")
+else:
+    print("该流行语暂未收录")
+input("收录的流行词：")
---
+++ 第三课时/text3.py
@@ -0,0 +1,7 @@
+contacts={('张伟',23):'4346756',
+          ('张伟',43):'4634634',
+          ('张伟',98):'4768767'}
+contacts['张伟',23]='4545454'
+del contacts[('张伟',43)]
+print(contacts)
+print(len(contacts))
